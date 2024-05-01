e1392f21a0fe425f0c43a784e4f3fc9994c06068
Sergio_Mac <badzega@gmail.com>
2024-05-01 13:44:14 +0200

    ADD: searchBar & types

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,15 +1,29 @@
 import React from "react";
+import { ImLocation2 } from "react-icons/im";
+import { MdMyLocation, MdWbSunny } from "react-icons/md";
+import SearchBar from "./SearchBar";
 
 type Props = {};
 
 export default function Navbar({}: Props) {
   return (
-    <nav className="top-0 left-0 sticky z-50 shadow-sm">
+    <nav className="top-0 left-0 sticky z-50 shadow-sm bg-white">
       <div className="flex justify-between items-center w-full h-[5rem] max-w-7xl px-3 mx-auto">
-        <p className="flex items-center justify-center gap-2">
+        <div className="flex items-center justify-center gap-2">
           {/* чого не H1? */}
           <h2 className="text-gray-500 text-3xl"> Weather</h2>
-        </p>
+          <MdWbSunny className="text-3xl mt-1 text-yellow-500" />
+        </div>
+        {/*  */}
+        <section className="flex items-center gap-2">
+          <MdMyLocation className="text-3xl text-gray-500 hover:opacity-80 cursor-pointer" />
+          <ImLocation2 className="text-3xl text-gray-500 hover:opacity-80 cursor-pointer" />
+          <p className="text-slate-900/80 text-lg">Poland</p>
+          <div>
+            {/* search */}
+            <SearchBar value={""} onChange={undefined} onSubmit={undefined} />
+          </div>
+        </section>
       </div>
     </nav>
   );
---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,38 @@
+import { cn } from "@/utils/cn";
+import React from "react";
+import { IoSearch } from "react-icons/io5";
+
+type SearchBarProps = {
+  className?: string;
+  value: string;
+  onChange: React.ChangeEventHandler<HTMLInputElement> | undefined;
+  onSubmit: React.FormEventHandler<HTMLFormElement> | undefined;
+};
+
+export default function SearchBar(props: SearchBarProps) {
+  return (
+    <form
+      onSubmit={props.onSubmit}
+      className={cn(
+        "flex relative items-center justify-center h-10",
+        props.className
+      )}
+    >
+      <input
+        onChange={props.onChange}
+        type="text"
+        placeholder="Search location..."
+        value={props.value}
+        className="border-solid border-2 border-gray-400 rounded-l-md shadow-md transition
+        focus:outline-none focus:border-zinc-600 px-4 py-2 w-60 h-full"
+      />
+      <button
+        className="bg-green-700 h-full px-4 py-2 rounded-r-md text-white
+      border-solid border-2 border-l-transparent border-gray-400 shadow-md transition
+      hover:bg-green-600 focus:bg-green-900 whitespace-nowrap"
+      >
+        <IoSearch className="text-lg" />
+      </button>
+    </form>
+  );
+}
